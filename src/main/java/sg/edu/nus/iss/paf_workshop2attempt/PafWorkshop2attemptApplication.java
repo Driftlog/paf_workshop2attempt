6b1a253e4718d78e602f99cc651ed6f963c78c7b
driftlog <driftlogged@gmail.com>
2023-06-25 03:44:22 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_workshop2attempt/PafWorkshop2attemptApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_workshop2attempt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafWorkshop2attemptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafWorkshop2attemptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_workshop2attempt/PafWorkshop2attemptApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_workshop2attempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafWorkshop2attemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
